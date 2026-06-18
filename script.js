5bb2b38f2826fbf788b01aa3c5c15a58a4315755
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-06-18 14:29:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-window.onload = () => {
+const themeBtn = document.getElementById("themeToggle");
+const langBtn = document.getElementById("langToggle");
 
-    const star = document.querySelector(".shooting-star");
+const title = document.getElementById("title");
+const text = document.getElementById("text");
 
-    // reinicio animación para asegurar timing
-    setTimeout(() => {
-        star.style.animation = "shoot 3s linear forwards";
-    }, 500);
+/* 🌙 DARK MODE */
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
 
-};
+    themeBtn.textContent =
+        document.body.classList.contains("light") ? "☀️" : "🌙";
+});
+
+/* 🌍 IDIOMA */
+let lang = "es";
+
+langBtn.addEventListener("click", () => {
+
+    if (lang === "es") {
+
+        title.textContent = "Hello, I'm Juana";
+        text.textContent = "Welcome to my portfolio";
+
+        langBtn.textContent = "EN";
+        lang = "en";
+
+    } else {
+
+        title.textContent = "Hola, soy Juana";
+        text.textContent = "Bienvenida a mi portfolio";
+
+        langBtn.textContent = "ES";
+        lang = "es";
+    }
+});
