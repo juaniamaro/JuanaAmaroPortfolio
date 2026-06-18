862e1c812380a6e094316ff5691d8dbc4a69205a
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-06-18 14:10:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+window.onload = () => {
+
+    const dot = document.querySelector(".light-dot");
+    const text = document.querySelector(".text");
+
+    setTimeout(() => {
+        text.style.filter = "drop-shadow(0 0 10px white)";
+    }, 2500);
+
+};
